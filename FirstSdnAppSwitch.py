0ba675eb93a8fa27ea2c809f1b750d2c37a4e141
jeb@elbrys.com <jeb@elbrys.com>
2015-08-23 15:23:42 -0400

    First try at PEP8 coding convention compliance

--- FirstSdnAppSwitch.py
+++ FirstSdnAppSwitch.py
@@ -10,27 +10,27 @@ import argparse
 import random
 import re
 
-def myNetwork(d,server,port):
 
-    net = Mininet( topo=None,
-                   build=False)
+def myNetwork(d, server, port):
 
-    info( '*** Adding controller\n' )
-    #net.addController(name='c0')
-    c1=net.addController(name='c0', controller=RemoteController, ip=server, port=port)
+    net = Mininet(topo=None,
+                  build=False)
 
-    info( '*** Add switches\n')
+    info('*** Adding controller\n')
+    # net.addController(name='c0')
+    c1 = net.addController(name='c0', controller=RemoteController, ip=server, port=port)
+
+    info('*** Add switches\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, dpid=d)
-    #s1 = net.addSwitch('s1', cls=OVSKernelSwitch, dpid='1')
-    Intf( 'eth2', node=s1 )
+    Intf('eth2', node=s1)
 
-    info( '*** Add hosts\n')
+    info('*** Add hosts\n')
     h1 = net.addHost('h1', ip='0.0.0.0')
 
-    info( '*** Add links\n')
+    info('*** Add links\n')
     net.addLink(h1, s1)
 
-    info( '*** Starting network\n')
+    info('*** Starting network\n')
     net.start()
     h1.cmdPrint('dhclient '+h1.defaultIntf().name)
     print " "
@@ -44,20 +44,32 @@ def myNetwork(d,server,port):
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
 
-    parser = argparse.ArgumentParser(description='Simple Mininet application to create and connect Mininet switch to ODL-S.')
-    parser.add_argument('--id',required=True,
-        help='your ODL-S Application id.  Go to sdn-developer.elbrys.com, logon, select "My Account" in top right.')
-    parser.add_argument('--server',required=True,
-        help='The IP address of your ODL-S server.  Go to sdn-developer.elbrys.com, logon, look at "Controller" table.')
-    parser.add_argument('--port',required=True,
-        help='The TCP port number of your ODL-S server.  Go to sdn-developer.elbrys.com, logon, look at "Controller" table.')
+    parser = argparse.ArgumentParser(description='Simple Mininet application\
+     to create and connect Mininet switch to ODL-S.')
+    parser.add_argument('--id',
+                        required=True,
+                        help='your ODL-S Application id.  Go to sdn-developer.\
+                        elbrys.com, logon, select "My Account" in top right.')
+    parser.add_argument('--server',
+                        required=True,
+                        help='The IP address of your ODL-S server.  Go to \
+                              sdn-developer.elbrys.com, logon, look at \
+                              "Controller" table.')
+    parser.add_argument('--port',
+                        required=True,
+                        help='The TCP port number of your ODL-S server.\
+                              Go to sdn-developer.\
+                              elbrys.com, logon, look at "Controller" table.')
     args = parser.parse_args()
+
+    # Create a random dpid that is constant based on input args
     random.seed(args.id)
-    r = random.randrange(0x111111111111,0xffffffffffff)
+    r = random.randrange(0x111111111111, 0xffffffffffff)
     r = r | 0x020000000000
     d = format(r, 'x')
     d = str(d)
-    
+
+    # Create the network of switches and endpoints
     myNetwork(d, args.server, args.port)
